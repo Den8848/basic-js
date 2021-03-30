6fdd4f8e04500cf447758579f4bafa16e5be7498
den8848 <denis2016wolverine2016@gmail.com>
2021-03-30 22:18:52 +0300

    feat: edit class depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,7 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
       if (Array.isArray(arr)) {
-          return 1 + Math.max(...arr.map(getArrayDepth)) 
+          return 1 + Math.max(...arr.map(calculateDepth)) 
       } else { 
           return 0
       }
