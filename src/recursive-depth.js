cb34db99e8408c27c7233056f0db737aa74df068
den8848 <denis2016wolverine2016@gmail.com>
2021-03-30 22:16:39 +0300

    feat: edit class depth calculator

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,20 +1,9 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr){
-      let levels = 1;
-      let prevLen = 1;
-      let currLen = arr.length;
-    
-      while(currLen > prevLen){
-        arr = arr.flat();
-        prevLen = currLen
-        currLen = arr.length;
-        levels++
+    calculateDepth(arr) {
+      if (Array.isArray(arr)) {
+          return 1 + Math.max(...arr.map(getArrayDepth)) 
+      } else { 
+          return 0
       }
-      return levels;
-    }
+      }
 };
-
-// const depthCalc = new DepthCalculator();
-// depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
-// depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
-// depthCalc.calculateDepth([[[]]]) => 3
